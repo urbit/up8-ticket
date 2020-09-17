09b21967baf45b6d682d95d0e4ba2b255727fd0e
Jared Tobin <jared@jtobin.io>
2020-09-16 21:53:36 -0230

    release: v0.1.1

--- README.md
+++ README.md
@@ -15,4 +15,13 @@ $ node --experimental-repl-await
 '~wisbyl-tarfes-biltug-datmyr-rigsyp-ribryc-nocmyr-bilres-mipset-patsut-todbur-foptev-lorfer-famtux-loppes-mismug-tobdyl-hopnes-lophul-tapdus-habtuc-ragseg-dossev-ramneb'
 ```
 
+If you're paranoid, you can use the `gen_custom` function to supply an
+additional custom buffer that will be XOR'd in with the generated entropy:
+
+```
+> let paranoia = await tg.gen(384, Buffer.from("a very random string"))
+> paranoia
+'~dolhes-parmes-tagnev-fablug-pagwyn-dopwel-ripnys-hardut-batnym-ridreb-finmec-mistes-figweg-labled-tocbet-bidryt-wolpub-filtev-tappeg-fassyt-tonred-savruc-lisred-tidlec'
+```
+
 [wgen]: https://github.com/urbit/urbit-wallet-generator
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const chunk = require('lodash.chunk')
 const flatMap = require('lodash.flatmap')
 const zipWith = require('lodash.zipwith')
 
+// generate a @q of the desired bitlength
 const gen = bits => {
   const bytes = bits / 8
   const some  = crypto.rng(bytes)
-
-  const prng = new more.Generator()
+  const prng  = new more.Generator()
 
   return new Promise((resolve, reject) => {
     prng.generate(bits, result =>  {
@@ -24,6 +24,30 @@ const gen = bits => {
   })
 }
 
+// generate a @q of the desired bitlength; the second argument should be a
+// Buffer that will be XOR'd with the generated entropy
+const gen_custom = (bits, addl) => {
+  const bytes = bits / 8
+  const some  = crypto.rng(bytes)
+
+  const prng = new more.Generator()
+
+  return new Promise((resolve, reject) => {
+    prng.generate(bits, result =>  {
+      const chunked = chunk(result, 2)
+      const desired = chunked.slice(0, bytes) // only take required entropy
+      const more    = flatMap(desired, arr => arr[0] ^ arr[1])
+      const moar    = zipWith(some, more, (x, y) => x ^ y)
+      const entropy = zipWith(moar, addl, (x, y) => x ^ y)
+      const buf     = Buffer.from(entropy)
+      const patq    = ob.hex2patq(buf.toString('hex'))
+      resolve(patq)
+      reject("entropy generation failed")
+    })
+  })
+}
+
 module.exports = {
-  gen: gen
+  gen,
+  gen_custom
 }
